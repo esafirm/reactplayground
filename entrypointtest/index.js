00ea1a14087f52ab0cbe022a2eab39d40f5824b6
Esa Firman <esafirm21@gmail.com>
2018-11-20 18:49:29 +0700

    Initial commit

---
+++ entrypointtest/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => (
+  <div className="test">
+    <h1>Halo halo Bandung</h1>
+  </div>
+);
+
+render(<App />, document.getElementById('root'));
